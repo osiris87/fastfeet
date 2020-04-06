b79c239bbc760c55ace2d6ac53cf66a88ad35847
Vasconcelos <osiris.d.vasconcelos@accenture.com>
2020-04-06 13:42:10 -0300

    feat: Adicionando arquivo .env

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,14 @@
+require("dotenv").config();
+
 module.exports = {
   dialect: "postgres",
-  host: "192.168.99.100",
-  username: "postgres",
-  password: "fastfeet",
-  database: "fastfeet",
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   define: {
     timestamps: true,
     underscored: true,
-    underscoredAll: true
-  }
+    underscoredAll: true,
+  },
 };
