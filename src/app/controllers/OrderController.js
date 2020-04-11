cec06bbf885e039d63dae78afae1dca00a3eddfc
Vasconcelos <osiris.d.vasconcelos@accenture.com>
2020-04-11 18:27:37 -0300

    feat: criacao da tabela order e signature

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -1,6 +1,27 @@
+import * as Yup from "yup";
+import Recipient from "./../models/Recipients";
+import Deliveryman from "./../models/Deliveryman";
+import Order from "../models/Order";
+
 class OrderController {
   async store(req, res) {
-    return res.json({ ok: true });
+    //const schema = Yup.object()
+
+    const { recipient_id, deliveryman_id } = req.body;
+
+    const recipient = await Recipient.findByPk(recipient_id);
+    if (!recipient) {
+      return res.status(401).json({ error: "Destinatario não encontrado" });
+    }
+
+    const deliveryman = await Deliveryman.findByPk(deliveryman_id);
+    if (!deliveryman) {
+      return res.status(401).json({ error: "Entregador não encontrado" });
+    }
+
+    const order = await Order.create(req.body);
+
+    return res.json(order);
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
 import RecipientsController from "./app/controllers/RecipientsController";
 import DeliverymanController from "./app/controllers/DeliverymanController";
+import OrderController from "./app/controllers/OrderController";
 import FileController from "./app/controllers/FileController";
 
 import authMiddleware from "./app/middlewares/auth";
 
 import multer from "multer";
 import multerConfig from "./config/multer";
+import SignatureController from "./app/controllers/SignatureController";
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -19,6 +21,7 @@ routes.post("/auth", SessionController.store);
 routes.use(authMiddleware);
 
 routes.post("/files", upload.single("file"), FileController.store);
+routes.post("/signature", upload.single("file"), SignatureController.store);
 
 routes.post("/recipients", RecipientsController.store);
 routes.put("/recipients/:recipient_id", RecipientsController.update);
@@ -27,5 +30,7 @@ routes.get("/deliveryman", DeliverymanController.index);
 routes.post("/deliveryman", DeliverymanController.store);
 routes.put("/deliveryman/:deliveryman_id", DeliverymanController.update);
 routes.delete("/deliveryman/:deliveryman_id", DeliverymanController.delete);
+
+routes.post("/orders", OrderController.store);
 
 export default routes;
